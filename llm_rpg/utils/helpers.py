25665892d7b870ff74d7c54722092249f473c38e
Stanislav Filippov <stanislav.filippov@gmail.com>
2025-02-23 21:20:51 +0100

    Added logger

--- llm_rpg/utils/helpers.py
+++ llm_rpg/utils/helpers.py
@@ -1,4 +1,6 @@
 from typing import Dict, List, Set, Union, Any
+import logging
+from sys import stdout
 
 def dict_2_str(d: Dict[str, str]) -> str:
     """
@@ -72,3 +74,29 @@ def parse_towns(loc_response: str, expected_fields: Union[List[str], Set[str]]) 
                         pass
 
     return locations
+
+
+def set_logger(level=logging.INFO,
+               fmt:str="[%(asctime)s] [%(name)8s] [%(levelname)-8s] %(message)s"):
+    """ Sets up a stdout logger """
+
+    logFormatter = logging.Formatter(
+        fmt=fmt
+    )
+
+    logger = logging.getLogger()
+    logger.setLevel(level)
+
+    # Remove any existing handlers to prevent duplicates in Jupyter
+    if logger.hasHandlers():
+        logger.handlers.clear()
+
+    # Create a StreamHandler that prints to sys.stdout (needed for Jupyter)
+    handler = logging.StreamHandler(stdout)
+    handler.setLevel(level)
+    handler.setFormatter(logFormatter)
+
+    # Add the handler to the logger
+    logger.addHandler(handler)
+
+    return logger
