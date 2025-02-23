9becceec4ae2601ef2b4ba73856b6e817e2d9c23
Stanislav Filippov <stanislav.filippov@gmail.com>
2025-02-23 21:22:46 +0100

    World generator class, ver 0.1

--- llm_rpg/engine/lore_generation.py
+++ llm_rpg/engine/lore_generation.py
@@ -1,37 +1,68 @@
+import logging
+logger = logging.getLogger(__name__)
+
 from llm_rpg.prompts.lore_generation import (kingdoms_traits,
-                                             world_desc_discworld,
-                                             world_desc_grim,
+                                             KINGDOM_DESC_STRUCT,
+                                             TOWNS_DESC_STRUCT,
                                              gen_world_msgs,
                                              gen_kingdom_msgs,
                                              gen_towns_msgs)
-from llm_rpg.utils.helpers import (dict_2_str,
-                                   parse_kingdoms_response,
+from llm_rpg.utils.helpers import (parse_kingdoms_response,
                                    parse_towns,
                                    parse_world_desc)
 from llm_rpg.templates.base_client import BaseClient
-from typing import Dict, List, Set, Any, Union
 
 
 class GenerateWorld:
     def __init__(self, client: BaseClient, **kwargs):
         """Init the class. Not sure what shall be here"""
-        pass
+        self.client = client
+        self.game_lore = {}
+        self.game_gen_params = {}
+        # defaults
+        self.expected_flds_kingdoms_def = set(KINGDOM_DESC_STRUCT.keys())
+        self.expected_flds_towns_def = set(TOWNS_DESC_STRUCT.keys())
 
-    def gen_world(self, **kwargs):
+
+    def gen_world(self, world_desc: str, **client_kw):
         """
         Generates the world description. Provide world description
 
-        :param kwargs:
+        :param client_kw:
         :return:
         """
-        return
+        msg_world_gen = gen_world_msgs(world_desc)
+        raw_world_response = self.client.chat(msg_world_gen, **client_kw)
+        world_ai = parse_world_desc(raw_world_response['message'])
+        logger.info(f"Created world: {world_ai['name']}")
+        logger.debug(f"Prompt tokens: {raw_world_response['stats']['promt_tokens']}")
+        logger.debug(f"Eval tokens: {raw_world_response['stats']['eval_tokens']}")
+        self.game_lore['world'] = world_ai
+        self.game_gen_params['model'] = self.client.model_name
+        self.game_gen_params['world'] = msg_world_gen
 
-    def gen_kingdoms(self, num_kingdoms:int, kingdom_traits:str, **kwargs):
-        """ Generates kingdoms in the world. Provide number of kingdoms, their traits
+
+    def gen_kingdoms(self, num_kingdoms:int, kingdom_types:str|None=None, **client_kw):
         """
-        return
+        Generates kingdoms in the world. Provide number of kingdoms, their types
+        """
 
-    def gen_towns(self, num_towns, world, kingdoms):
+        if kingdom_types is None:
+            kingdom_types = kingdoms_traits
+        if kingdom_types is not None and kingdom_types == "":
+            kingdom_types = kingdoms_traits
+
+        kingdoms_msg = gen_kingdom_msgs(num_kingdoms, kingdom_types, self.game_lore['world'])
+        kingdoms_raw_response = self.client.chat(kingdoms_msg, **client_kw)
+        kingdoms_ai = parse_kingdoms_response(kingdoms_raw_response['message'], self.expected_flds_kingdoms_def)
+        logger.info(f"Created kingdoms: {list(kingdoms_ai.keys())}")
+        logger.debug(f"Prompt tokens: {kingdoms_raw_response['stats']['promt_tokens']}")
+        logger.debug(f"Eval tokens: {kingdoms_raw_response['stats']['eval_tokens']}")
+        self.game_lore['kingdoms'] = kingdoms_ai
+        self.game_gen_params['kingdoms'] = kingdoms_msg
+
+
+    def gen_towns(self, num_towns, **clinet_kw):
         """
         Generates towns for each kingdom. Provide number of towns.
         TBD: a single number for all kingdoms or introduce some variability. Issue with variability
@@ -42,9 +73,19 @@ class GenerateWorld:
         :param kingdoms:
         :return:
         """
-        return
 
+        self.game_lore['towns'] = {}
+        self.game_gen_params['towns'] = {}
 
+        for kingdom in self.game_lore['kingdoms']:
+            logger.info(f"Generating {num_towns} towns for {kingdom}")
+            msg_towns_k = gen_towns_msgs(num_towns, self.game_lore['world'], self.game_lore['kingdoms'], kingdom)
+            towns_raw_response = self.client.chat(msg_towns_k, **clinet_kw)
+            logger.debug(f"Prompt tokens: {towns_raw_response['stats']['promt_tokens']}")
+            logger.debug(f"Eval tokens: {towns_raw_response['stats']['eval_tokens']}")
+            towns = parse_towns(towns_raw_response['message'], self.expected_flds_towns_def)
+            self.game_lore['towns'][kingdom] = towns
+            self.game_gen_params['towns'][kingdom] = msg_towns_k
 
 
 
