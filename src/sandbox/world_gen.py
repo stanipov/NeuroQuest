d86646f76045d239cbb2c8c346300e5934808110
Stanislav Filippov <stanislav.filippov@gmail.com>
2026-06-16 22:27:35 +0200

    WIP: prompt optimization

--- src/sandbox/world_gen.py
+++ src/sandbox/world_gen.py
@@ -9,15 +9,19 @@ from loguru import logger
 async def main():
     rules, narrative, player_card, npc_card = None, None, None, None
 
-    world_type = WorldTypes(type="dystopian dark fantasy",
-                            inspiration="Orwell's 1984 totalitarianism meets brutality of Game of Thrones and \
-                            horrors of cosmic dread, where ancient curses and forgotten gods twist \
-                            the natural order")
+    # world_type = WorldTypes(type="dystopian dark fantasy",
+    #                         inspiration="Orwell's 1984 totalitarianism meets brutality of Game of Thrones and \
+    #                         horrors of cosmic dread, where ancient curses and forgotten gods twist \
+    #                         the natural order")
+
+    world_type = WorldTypes(type="high fantasy",
+                            inspiration="Political intrigues of Game of Thrones combined with epic fantasy world")
+
     logger.info(f"Creating a world rules for {world_type}")
     rules = await b.GenWorldRules(world_type)
 
     logger.info(f"Creating the narrative")
-    narrative = await b.NarrateWorld(rules)
+    narrative = await b.NarrateWorld(world_type, rules)
 
     # logger.info("Creating the player's card")
     # player_card = await b.GenPlayerCard(world_type, rules, narrative)
